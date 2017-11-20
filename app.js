1ae860984fac5a4b51dc4e96a4dad74b091ce364
snayrouz <samuel.nayrouz@gmail.com>
2017-11-20 11:59:51 -0700

    Add event listener for click

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ var UIController = (function() {
 })();
 
 // GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UTCtrl) {
-  document.querySelector('.add__btn').addEventListener('click', function() {
+var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function(){
     // 1. Get the filled input data
     // 2. add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Caluculate the budget
     // 5. Display the budeget on the UI
-  });
+    console.log('It works')
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
+
     if (event.keyCode === 13) {
-      console.log('ENTER was pressed.');
+      ctrlAddItem();
     }
   });
 
