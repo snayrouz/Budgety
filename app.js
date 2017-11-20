5bc50e7d2c11ffae6d8eb13efd7b07157e60223d
snayrouz <samuel.nayrouz@gmail.com>
2017-11-20 11:48:09 -0700

    Add layout

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
-
+  //some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
   // some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UTCtrl) {
-
-  var z = budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
-  }
-
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    // 1. Get the filled input data
+    // 2. add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Caluculate the budget
+    // 5. Display the budeget on the UI
+  });
 })(budgetController, UIController);
