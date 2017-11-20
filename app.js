9196d5453dc76eefbc334365b52d142f04cfe2d4
snayrouz <samuel.nayrouz@gmail.com>
2017-11-20 12:09:29 -0700

    Add input object

--- app.js
+++ app.js
@@ -5,18 +5,27 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-  // some code
+  return {
+    getinput: function() {
+      return {
+         type: document.querySelector('.add__type').value, // will be either inc or exp
+         description: document.querySelector('.add__description').value,
+         value: document.querySelector('.add__value').value,
+      };
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
   var ctrlAddItem = function(){
     // 1. Get the filled input data
+    var input = UICtrl.getinput();
+    console.log(input);
     // 2. add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Caluculate the budget
     // 5. Display the budeget on the UI
-    console.log('It works')
   }
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
@@ -26,6 +35,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     if (event.keyCode === 13) {
       ctrlAddItem();
     }
+
   });
 
 })(budgetController, UIController);
