4983cb45e34856907fbac0036511edb6765277c4
snayrouz <samuel.nayrouz@gmail.com>
2017-11-21 16:13:27 -0700

    Add private functios in budget controller

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
-  //some code
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
 })();
 
 // UI CONTROLLER
